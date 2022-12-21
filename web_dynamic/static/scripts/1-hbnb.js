caae96a233f811db0cbb109273464248818f5761
Bester <jol.alvarezc@gmail.com>
2022-12-21 11:40:12 -0300

    js 1task

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,5 +1,12 @@
 window.onload = function () {
-    const checkbox = document.querySelector('');
-
-
-}
+  const checkbox = document.querySelector('#checkbox');
+  checkbox.addEventListener('change', function () {
+    let list_checked = [];
+    if (checkbox.checked) {
+      list_checked.appendChild(amenities.id);
+    } else if (!checkbox.checked) {
+      list_checked.removeChild(amenities.id);
+    }
+  });
+  }
+  console.log(checkbox);
