eee073e277c4bf885055c5a67af087d84f127c65
Damian25121997 <damiancaetano25@gmail.com>
2022-12-21 15:18:23 -0300

    Update task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,31 @@
 window.onload = function () {
-  const checkbox = document.querySelector('#checkbox');
-  checkbox.addEventListener('change', function () {
-    let list_checked = [];
-    if (checkbox.checked) {
-      list_checked.appendChild(amenities.id);
-    } else if (!checkbox.checked) {
-      list_checked.removeChild(amenities.id);
-    }
-  });
+  const checkbox = document.querySelector('#sad');
+  const listofchilds = checkbox.children;
+  const title = document.querySelector('.amenities h4');
+  const list_checked = [];
+  for (const x of listofchilds) {
+    const input = x.firstElementChild;
+    input.addEventListener('change', function () {
+      if (input.checked) {
+        list_checked.push(input.getAttribute('data-name'));
+      } else {
+        const index = list_checked.indexOf(input.getAttribute('data-name'));
+        list_checked.splice(index, 1);
+      }
+      let string = '';
+      let count = 0;
+      for (const i of list_checked) {
+        if (count === list_checked.length - 1) {
+          string += i;
+        } else {
+          string += i + ', ';
+        }
+        count += 1;
+      }
+      if (string.length >= 27) {
+        string = string.substring(0, 27) + '...';
+      }
+      title.textContent = string;
+    });
   }
-  console.log(checkbox);
+};
